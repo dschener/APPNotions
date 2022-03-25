42bee394ced54f08dc4001e1a8ef84f859d36cdc
davidnotions <99275156+davidnotions@users.noreply.github.com>
2022-03-25 14:56:17 -0300

    url login de cual es mi ip

--- APPNotions/MainPage.xaml.cs
+++ APPNotions/MainPage.xaml.cs
@@ -19,40 +19,40 @@ namespace APPNotions
             InitializeComponent();
 
 
-            //if (string.IsNullOrEmpty(url))
-            //{
-            //    Navegador.Source = Config.urlLogin;
-            //}
-            //else
-            //{
-            //    Navegador.Source = url;
-            //}
+            if (string.IsNullOrEmpty(url))
+            {
+                Navegador.Source = Config.urlLogin;
+            }
+            else
+            {
+                Navegador.Source = url;
+            }
         }
 
-        //async void Navegador_Navigating(object sender, WebNavigatingEventArgs e)
-        //{
-        //    if (e.Url.Contains("logout"))
-        //    {
-        //        OneSignal.Default.SendTag("tokenUsuario", "logout");
-        //        Navegador.Source = Config.urlLogin;
-        //    }
-        //    if (e.Url.Contains("token"))
-        //    {
-        //        string token = e.Url.Split(new string[] { "token=" }, StringSplitOptions.None)[1];
-        //        OneSignal.Default.SendTag("tokenUsuario", token);
-        //    }
-        //    if (e.Url.Contains("target=browser") || e.Url.StartsWith("tel:"))
-        //    {
-        //        try
-        //        {
-        //            await Launcher.OpenAsync(new Uri(e.Url));
-        //            e.Cancel = true;
-        //        }
-        //        catch
-        //        {
+        async void Navegador_Navigating(object sender, WebNavigatingEventArgs e)
+        {
+            if (e.Url.Contains("logout"))
+            {
+                OneSignal.Default.SendTag("tokenUsuario", "logout");
+                Navegador.Source = Config.urlLogin;
+            }
+            if (e.Url.Contains("token"))
+            {
+                string token = e.Url.Split(new string[] { "token=" }, StringSplitOptions.None)[1];
+                OneSignal.Default.SendTag("tokenUsuario", token);
+            }
+            if (e.Url.Contains("target=browser") || e.Url.StartsWith("tel:"))
+            {
+                try
+                {
+                    await Launcher.OpenAsync(new Uri(e.Url));
+                    e.Cancel = true;
+                }
+                catch
+                {
 
-        //        }
-        //    }
-        //}
+                }
+            }
+        }
     }
 }
