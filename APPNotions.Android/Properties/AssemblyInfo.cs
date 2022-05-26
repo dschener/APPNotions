7f237d9a6ed691cd78630172b4b4aac069d700a0
davidnotions <99275156+davidnotions@users.noreply.github.com>
2022-05-26 09:19:28 -0300

    MovPark Concepcion con imagenes

--- APPNotions.Android/Properties/AssemblyInfo.cs
+++ APPNotions.Android/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly
-[assembly: AssemblyTitle("com.vecinodigitaldesarrollo")]
+[assembly: AssemblyTitle("com.movilparkingconcepcion")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("com.vecinodigitaldesarrollo")]
+[assembly: AssemblyProduct("com.movilparkingconcepcion")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
